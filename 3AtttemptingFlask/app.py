486109b8a22181fd1d2eb5bc433cc26487db8245
Cass <cnellis@alumni.unc.edu>
2019-01-11 02:51:55 -0500

    view plots using flask

--- 3AtttemptingFlask/app.py
+++ 3AtttemptingFlask/app.py
@@ -8,12 +8,12 @@ import numpy as np
 # from sqlalchemy.orm import Session
 # from sqlalchemy import create_engine
 
+from flask_cors import CORS
 from flask import Flask, jsonify, render_template, url_for
 # from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
+CORS(app)
 
 pieData = pd.read_csv("pieData.csv") 
 barData = pd.read_csv("barData.csv")
@@ -34,31 +34,23 @@ USOData = pd.read_csv("USOnly.csv")
 # Create a route that renders index.html template
 @app.route("/")
 def index():
-    # print(USOData.head())
-    # print(barData.head())
-    # print(pieData.head())
-
     return render_template("index.html")
 
 # Query the line data and return the jsonified results
 @app.route("/line")
 def line():
-    
     USOdict = USOData.to_dict(orient="record")
     return jsonify(USOdict)
 
 @app.route("/bar")
 def bar():
-    
     barDict = barData.to_dict(orient="record")
     return jsonify(barDict)
 
 @app.route("/pie")
 def pie():
-
     pieDict = pieData.to_dict(orient="record")
     return jsonify(pieDict)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
--- 3AtttemptingFlask/static/js/plots.js
+++ 3AtttemptingFlask/static/js/plots.js
@@ -3,36 +3,34 @@
 // -----------------------------------------------------
 // console.log("HEY IM RUNNING IN FLASK!")
 
-
-function buildPlot1() {
-  var url = "/line";
+function buildLine() {
+  var url = "http://localhost:5000/line";
   // use 'd3.json' to etch the sample data for the plots
-    d3.json(url).then(function(response) {
-      // console.log(response);
-      var accident = []
-      var alzheimers = []
-      var clrd = []
-      var cancer = []
-      var diabetes = []
-      var heart = []
-      var influenza = []
-      var kidney = []
-      var suicide = []
-      var stroke = []
-      var year = []      
+  d3.json(url).then(function(response) {
+      var accident = [];
+      var alzheimers = [];
+      var clrd = [];
+      var cancer = [];
+      var diabetes = [];
+      var heart = [];
+      var influenza = [];
+      var kidney = [];
+      var suicide = [];
+      var stroke = [];
+      var year = [];   
 
       response.forEach(d => {
-        accident.push(d["Accident"])
-        alzheimers.push(d["Alzheimer's Disease"])
-        clrd.push(d["CLRD"])
-        cancer.push(d["Cancer"])
-        diabetes.push(d["Diabetics "])
-        heart.push(d["Heart"])
-        influenza.push(d["Influenza"])
-        kidney.push(d["Kidney"])
-        suicide.push(d["Suicide"])
-        stroke.push(d["Stroke"])
-        year.push(d["Year"])
+        accident.push(d["Accident"]);
+        alzheimers.push(d["Alzheimer's Disease"]);
+        clrd.push(d["CLRD"]);
+        cancer.push(d["Cancer"]);
+        diabetes.push(d["Diabetics "]);
+        heart.push(d["Heart"]);
+        influenza.push(d["Influenza"]);
+        kidney.push(d["Kidney"]);
+        suicide.push(d["Suicide"]);
+        stroke.push(d["Stroke"]);
+        year.push(d["Year"]);
       })
 
       var trace1 = {
@@ -41,8 +39,10 @@ function buildPlot1() {
         mode: "markers",
         type: "scatter",
         name: "Alzheimer's Disease",
-        marker: {color: "#2077b4", symbol: "hexagram"}
-      }
+        marker: {
+          color: "#2077b4", 
+          symbol: "hexagram"
+        }
       };
       var trace2 = {
         x: year,
@@ -66,39 +66,114 @@ function buildPlot1() {
           symbol: "cross"
         }
       };
-      // // Create the data array for the plot
-      var data = [trace1, trace2, trace3];
-      // // Define the plot layout
-      var layout = {
-        title: "Change in Casues of Death Over the Years",
-        xaxis: { title: "Year" },
-        yaxis: { title: "Cause of Death" }
+      var trace4 = {
+        x: year,
+        y: diabetes,
+        mode: "markers",
+        type: "scatter",
+        name: "Diabetes",
+        marker: {
+          color: "red",
+          symbol: "cross"
+        }
       };
-      // // Plot the chart to a div tag with id "plot"
-      Plotly.newPlot("line", lineData, layout);
+      var trace5 = {
+        x: year,
+        y: heart,
+        mode: "markers",
+        type: "scatter",
+        name: "Heart Disease",
+        marker: {
+          color: "blue",
+          symbol: "cross"
+        }
+      };
+      var trace6 = {
+        x: year,
+        y: influenza,
+        mode: "markers",
+        type: "scatter",
+        name: "Influenza",
+        marker: {
+          color: "green",
+          symbol: "cross"
+        }
+      };
+      var trace7 = {
+        x: year,
+        y: kidney,
+        mode: "markers",
+        type: "scatter",
+        name: "Kidney",
+        marker: {
+          color: "purple",
+          symbol: "cross"
+        }
+      };
+      var trace8 = {
+        x: year,
+        y: stroke,
+        mode: "markers",
+        type: "scatter",
+        name: "Stroke",
+        marker: {
+          color: "aqua",
+          symbol: "cross"
+        }
+      };
+      var trace9 = {
+        x: year,
+        y: suicide,
+        mode: "markers",
+        type: "scatter",
+        name: "Suicide",
+        marker: {
+          color: "gold",
+          symbol: "cross"
+        }
+      };
+      var trace10 = {
+        x: year,
+        y: accident,
+        mode: "markers",
+        type: "scatter",
+        name: "Accident",
+        marker: {
+          color: "tomato",
+          symbol: "cross"
+        }
+      };
+      // Create the data array for the plot
+      var data = [trace1, trace2, trace3, trace4, trace5, trace6, trace7, trace8, trace9, trace10];
+      // Define the plot layout
+      var layout = {
+        xaxis: { title: "Year" },
+        yaxis: { title: "Death Rate (per 100,000 people)" }
+      };
+      // Plot the chart to a div tag with id "plot"
+      Plotly.newPlot("line", data, layout);
   });
 }
-buildPlot1();
+buildLine();
+
 
 // bar chart -------------------------------------------
 // reference: https://plot.ly/javascript/bar-charts/
 // also look into Week 15, Day 1, Activity 
 // Activity 16:2:3 - Bar Chart from csv
-var url = "/bar";
-function buildPlot2() {
+function buildBar() {
+  var url = "http://localhost:5000/bar";
   d3.json(url).then(function(response) {
-    console.log(response); 
-    var cause = []
-    var rate1999 = []
-    var rate2008 = []
-    var rate2016 = []
-      response.forEach(d => {
-        cause.push(d["Cause"])
-        rate1999.push(d["Rates1999"])
-        rate2008.push(d["Rates2008"])
-        rate2016.push(d["Rates2016"])
-      })
-
+    var cause = [];
+    var rates1999 = [];
+    var rates2008 = [];
+    var rates2016 = [];
+    response.forEach(d => {
+      cause.push(d["Cause"]);
+      rates1999.push(d["Rates1999"]);
+      rates2008.push(d["Rates2008"]);
+      rates2016.push(d["Rates2016"]);
+    })
     function initBar() {
       var barData = [{x: cause, y: rates2016, type: 'bar'}];
       var layout = {height: 400, width: 500};
@@ -121,30 +196,31 @@ function buildPlot2() {
       updatePlotly(data);
     }
     initBar();
-   Plotly.newPlot('bar', barData);
   });
 }
-buildPlot2();
+buildBar();
+
 // ------------------------------------------------------
- 
+
 // pie chart --------------------------------------------
 // reference: https://plot.ly/javascript/pie-charts/ 
 // make interactive with dropdown events: look at week 15, day 2, activity 2
-var url = "/pie";
-function buildPlot3() {
-  d3.json(url).then(function(response) {
-    console.log(response); 
-
+function buildPie() {
+  var url = "http://localhost:5000/pie";
+  d3.json(url).then(function(response) { 
+    var cause = [];
+    var percents1999 = [];
+    var percents2008 = [];
+    var percents2016 = [];
+    response.forEach(d => {
+      cause.push(d["Cause"]);
+      percents1999.push(d["Percents1999"]);
+      percents2008.push(d["Percents2008"]);
+      percents2016.push(d["Percents2016"]);
+    })
     function initPie() {
-      var data = [{
-        values: data4.Percents2016,
-        labels: data4.Cause,
-        type: 'pie'
-      }];
-      var layout = {
-        height: 400,
-        width: 500
-      };
+      var data = [{values: percents2016, labels: cause, type: 'pie'}];
+      var layout = {height: 400, width: 500};
       Plotly.plot('pie', data, layout);
     }
       //define update plot function
@@ -156,17 +232,16 @@ function buildPlot3() {
       function getData(dataset) {
         var data = [];
         switch (dataset) {
-        case "dataset1": data = data4.Percents2016; break;
-        case "dataset2": data = data4.Percents2008; break;
-        case "dataset3": data = data4.Percents1999; break;
-        default: data = data4.Percents2016; 
+        case "dataset1": data = percents2016; break;
+        case "dataset2": data = percents2008; break;
+        case "dataset3": data = percents1999; break;
+        default: data = percents2016; 
         }
         updatePlotly(data);
       }
     initPie();
-
-    Plotly.newPlot('pie', pieData, layout);
+    Plotly.newPlot('pie', data, layout);
   });
 }
-buildPlot3();
+buildPie();
 // ---------- --------------------------------------------
